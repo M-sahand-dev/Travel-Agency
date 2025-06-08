102453105c1ad211efd6fd0d86b3a26b00db5873
M-sahand-dev <leymehdi5@gmail.com>
2025-06-08 17:21:08 +0330

    -Add font file and install -install react-icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+
 function App() {
   return (
     <>
+      <h1 className="text-4xl font-bold text-center mt-10 mb-10">ایران سنس</h1>
     </>
-  )
+  );
 }
 
 export default App
