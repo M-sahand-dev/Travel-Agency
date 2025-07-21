c12122e3bd4c047d7c45fdcbeaae378bf9ae95dc
M-sahand-dev <leymehdi5@gmail.com>
2025-07-21 18:54:50 +0330

    add font style body and add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import "./App.css";
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="App">
+        <h1>Travel Agency</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
