9b22663814d03a2f3a6112b8fcdfb9c64e1182fd
M-sahand-dev <leymehdi5@gmail.com>
2025-07-23 16:01:53 +0330

    creating srtyle heroslider | img

--- src/components/landingPage/heroSection/HeroImg.tsx
+++ src/components/landingPage/heroSection/HeroImg.tsx
@@ -4,21 +4,21 @@ import { FaArrowRight } from "react-icons/fa6";
 
 export const HeroImg = (): JSX.Element => {
   return (
-    <div className="w-[636px] h-[808px] relative">
+    <div className="w-[636px] h-[808px] relative text-white">
       <img
-        src={"/public/image/place/antalia.png"}
+        src={"/public/image/place/pataya.png"}
         className="w-full h-full rounded-5xl absolute"
         alt=""
       />
       <div className="relative font-Rokh">
-        <div className="">
-          <span>
+        <div className="bg-white-tertiary border rounded-4xl py-6 px-8 absolute top-4 left-4 border-gradent-primary ">
+          <span className="flex items-center gap-[2px] font-bold text-md">
             <CiLocationOn />
             تایلند
           </span>
-          <h3>پاتایا</h3>
+          <h3 className="font-bold text-2xl text-76">پاتایا</h3>
         </div>
-        <div className="">
+        <div className="absolute top-52 left-12 text-5xl flex items-center justify-between">
           <button>
             <FaArrowRight />
           </button>
