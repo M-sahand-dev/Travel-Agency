eb6082e003783117cc815ebe51a77a6c6ff28441
M-sahand-dev <leymehdi5@gmail.com>
2025-08-04 14:35:23 +0330

    `Added loading state control and preloader to DetailPage component`

--- src/components/detailPage/DetailPage.tsx
+++ src/components/detailPage/DetailPage.tsx
@@ -1,9 +1,33 @@
-import { type JSX } from "react";
+import { type JSX, useEffect, useState } from "react";
 import { Footer, Header } from "../index";
 import { ProductDetails, ProductTour } from "./index";
+import { Preloader } from "../ui"; // Adjust the path if necessary
 
 export const DetailPage = (): JSX.Element => {
-  return (
+  // Loading state control
+  const [loading, setLoadig] = useState(true);
+  const fetchingData = async () => {
+    return new Promise<void>((resolve) => {
+      const timer = setTimeout(() => {
+        setLoadig(false);
+        resolve();
+      }, 2000); // 2 seconds delay
+
+      // Cleanup function to prevent memory leaks
+      return () => clearTimeout(timer);
+    });
+  };
+  useEffect(() => {
+    fetchingData();
+    // Add 'loaded' class to body when loading completes
+    if (!loading) {
+      document.body.classList.add("loaded");
+    }
+  }, [loading]);
+
+  return loading ? (
+    <Preloader />
+  ) : (
     <div className="">
       <Header />
       <div className="py-20 px-12 container mx-auto">
